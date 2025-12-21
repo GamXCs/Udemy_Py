55b4713f033e6ce6d17582d13c65a838ac7c17be
Walter Stewart <gavlevi@uab.edu>
2025-12-20 21:50:34 -0600

    gradebook

--- gradebook.py
+++ gradebook.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 gradebook = {
     "Ava": [88, 92, 79],
     "Ben": [100, 73],
@@ -6,9 +8,25 @@ gradebook = {
     "Eli": [65, 70, 72],
 }
 
+
+# Function to calculate letter grade
+def grade_calc(average):
+    if average >= 90:
+        return "A"
+    elif average >= 80:
+        return "B"
+    elif average >= 70:
+        return "C"
+    elif average >= 60:
+        return "D"
+    else:
+        return "F"
+
+
 # Add new grade
 name = "Gam"
 new_grade = 98
+summary = {}
 
 gradebook.setdefault(name, []).append(new_grade)
 print(gradebook)
@@ -18,8 +36,22 @@ for name, grades in gradebook.items():
     # If there are no grades in the list, skip to next
     if len(grades) == 0:
         continue
-    # Compute average
-    average = sum(grades) / len(grades)
-    print(name, average)
 
-# def gradeCalc(grade):
+    # Compute values to store in summary dictionary
+    average = sum(grades) / len(grades)
+    letter = grade_calc(average)
+    minimum = min(grades)
+    maximum = max(grades)
+    count = len(grades)
+
+    summary[name] = {
+        "avg": average,
+        "grade": letter,
+        "lowest grade": minimum,
+        "highest grade": maximum,
+        "count": count,
+    }
+
+pprint(summary)
+
+# Build a summary dictionary
