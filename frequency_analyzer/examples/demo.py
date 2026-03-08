39bbf98959a867d5122bd06477dcb38b6a7aa9fb
Walter Stewart <gavlevi@uab.edu>
2026-03-08 14:03:27 -0500

    freq counter for words

---
+++ frequency_analyzer/examples/demo.py
@@ -0,0 +1,6 @@
+from frequency_analyzer.analyzer import word_frequency
+
+text1 = "to be or not to be"
+text2 = "Hello, world! Hello."
+print(word_frequency(text1))
+print(word_frequency(text2))
--- frequency_analyzer/src/frequency_analyzer/analyzer.py
+++ frequency_analyzer/src/frequency_analyzer/analyzer.py
@@ -1,5 +1,31 @@
+import string
+
 """
 Frequency Analyzer
 
 Provides tools for analyzing word and character frequencies in text.
 """
+
+
+def word_frequency(text):
+
+    # Normalize
+    text = text.lower()
+
+    # get rid of punctuation w/ string library
+    for char in string.punctuation:
+        text = text.replace(char, "")
+
+    # turn into a list of words
+    words = text.split()
+
+    # create dictionary to store results
+    new_dict = {}
+
+    # loop through string and create a count
+    for word in words:
+        if word in new_dict:
+            new_dict[word] += 1
+        else:
+            new_dict[word] = 1
+    return new_dict
